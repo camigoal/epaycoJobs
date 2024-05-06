f8717fa04d7523d251819d964af4d559f2fb5559
camigoal <camila.gomeza@uniagustiniana.edu.co>
2024-05-06 17:14:14 -0500

    Subiendo ePayco Jobs

---
+++ app.js
@@ -0,0 +1,14 @@
+/*'use strict'
+
+const grande = document.querySelector('.grande')
+const punto = documen.querySelectorAll('.punto')
+
+punto.array.forEach((cadaPunto, i) => {
+    punto[i].addEventListener('click', ()=>{
+        let posicion = i
+        let operacion = posicion * -50
+
+        grande.style.transform = translateX(-50%) 
+    })
+    
+});*/
